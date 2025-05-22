df74dc33b7f8dd6c35bee6b7f9045d17855316c7
Prabhashperera <prabashmperera01@gmail.com>
2025-05-22 11:24:56 +0530

    first commit

---
+++ MIMETypes/src/main/java/org/example/MIMETypesServlet.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+@WebServlet("/home")
+public class MIMETypesServlet extends HttpServlet {
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+////        Read Text/plain data from http request body
+//        String body = new BufferedReader(new InputStreamReader(req.getInputStream())).lines().collect(Collectors.joining("\n"));
+//        resp.setContentType("text/plain");
+//        resp.getWriter().println(body);
+//    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        resp.setContentType("text/plain");
+        resp.getWriter().print("Hello " + name + " " + address);
+    }
+}
