dc32e2e553b7df2d97a1da00658c87121b485e66
Prabhashperera <prabashmperera01@gmail.com>
2025-05-22 11:40:27 +0530

    form-data - Multipart

--- MIMETypes/src/main/java/org/example/MIMETypesServlet.java
+++ MIMETypes/src/main/java/org/example/MIMETypesServlet.java
@@ -1,17 +1,20 @@
 package org.example;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
-@WebServlet("/home")
+@MultipartConfig
+@WebServlet("/")
 public class MIMETypesServlet extends HttpServlet {
 //    @Override
 //    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -22,11 +25,21 @@ public class MIMETypesServlet extends HttpServlet {
 //    }
 
 
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String name = req.getParameter("name");
+//        String address = req.getParameter("address");
+//        resp.setContentType("text/plain");
+//        resp.getWriter().print("Hello " + name + " " + address);
+//    }
+
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        String address = req.getParameter("address");
+        Part img = req.getPart("img");
+        String submittedFileName = img.getSubmittedFileName();
         resp.setContentType("text/plain");
-        resp.getWriter().print("Hello " + name + " " + address);
+        resp.getWriter().write(submittedFileName + " has been submitted, " + name);
     }
 }
